130955aa09eefcddda1bb357638423d60d0c846c
ryxios <nowak.joel@gmail.com>
2025-08-26 08:57:07 +0200

    Refactor main module orchestration

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import { fetchGames } from './api.js';
-import { initUI } from './ui.js';
-import { generateImage } from './imageGenerator.js';
-
-export function startApp() {
-  $(document).ready(function () {
-    initUI(fetchGames, generateImage);
-  });
-}
